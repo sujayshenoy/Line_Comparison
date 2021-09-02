5d479fa53a1bbe7baae0f208f651c15548f4c504
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-02 09:03:57 +0530

    line comparison method moved inside Point class

--- src/main_package/Point.java
+++ src/main_package/Point.java
@@ -16,7 +16,26 @@ public class Point {
 		double y2 = p.y;
 
 		double length = Math.sqrt(Math.pow(x2-x1, 2) +  Math.pow(y2-y1, 2));
-		
+
 		return length;
 	}
+
+	public static void compareLine(Double length1, Double length2){
+		if(length1.equals(length2)) {
+			System.out.println("Both Lines l1 and l2 are equal");
+		}
+		else {
+			int res = length1.compareTo(length2);
+
+			switch (res) {
+			case 1:
+				System.out.println("Line l1 is longer than line l2");
+				break;
+			case -1:
+				System.out.println("Line l2 is longer than line l1");
+				break;
+			}
+		}
+	}
+
 }
